2ed4b32dce630d198d63abb93a86726fe4e9bc20
jens-a-e <jens@ififelse.net>
2012-03-28 23:05:06 +0200

    Added specs for key keeping

--- specs/specialsort-spec.php
+++ specs/specialsort-spec.php
@@ -3,11 +3,17 @@ include '../lib/minitest/src/MiniSpec/autoload.php';
 include '../src/specialsort.php';
 
 describe("Testing correctly sorted Umalute", function(){
-  it("sorts correctly", function() {
-    $source = array("Ügistan","Deutschland","italien","Israel","ömröm","Ägypten");
-    $target = array("Ägypten","Deutschland","Israel","italien","ömröm","Ügistan");
+  it("Sorts values correctly", function() {
+    $source = array("Ügistan","Deutschland","Dänemark","italien","Israel","ömröm","Ägypten");
+    $target = array("Ägypten","Dänemark","Deutschland","Israel","italien","ömröm","Ügistan");
     specialsort($source);
     expect(array_values($source))->toBe(array_values($target));
+  });
+  it("Sorts correctly with keys", function() {
+    $source = array("UE"=>"Ügistan","DE"=>"Deutschland","DK"=>"Dänemark","I"=>"italien","IS"=>"Israel","OE"=>"ömröm","E"=>"Ägypten");
+    $target = array("E"=>"Ägypten","DK"=>"Dänemark","DE"=>"Deutschland","IS"=>"Israel","I"=>"italien","OE"=>"ömröm","UE"=>"Ügistan");
+    specialsort($source);
+    expect($source)->toBe($target);
   });
 });
 
